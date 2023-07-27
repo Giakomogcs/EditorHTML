c158a67ff5d88afa9d6782be4e6d2841fb62ac38
Giovani <giakomogcs@gmail.com>
2023-07-26 22:13:21 -0300

    desenhando menu

--- Menu.cs
+++ Menu.cs
@@ -2,13 +2,44 @@ using System;
 
 namespace EditorHTML{
   public static class Menu{
-    public static void Show(){
+    public static void Show(){   
       Console.Clear();
       Console.BackgroundColor = ConsoleColor.Black;
       Console.ForegroundColor = ConsoleColor.DarkRed;
-
-      Console.WriteLine("Menu...");
+      
+      Console.WriteLine("Menu..."); 
+      DrawScreen();
     }
+    public static void Row(){
+      Console.Write("+");
+
+      for(int i=0; i<=30; i++){
+        Console.Write("-");
+      }
+
+      Console.Write("+");
+      Console.Write("\n");
+    }
+
+    public static void Colum(){
+      for(int lines=0; lines<=10; lines++){
+        Console.Write("|");
+
+        for(int i=0; i<=30; i++){
+          Console.Write(" ");
+        }
+
+        Console.Write("|");
+        Console.Write("\n");
+      }
+    }
+    public static void DrawScreen(){
+      Row();
+      Colum();
+      Row();
+    }
+
   }
+
 
 }
