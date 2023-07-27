fca4607b036e0959b476ad6d78059878aea543f6
Giovani <giakomogcs@gmail.com>
2023-07-27 20:43:48 -0300

    escrevendo opções menu

--- Menu.cs
+++ Menu.cs
@@ -37,6 +37,30 @@ namespace EditorHTML{
       Row();
       Colum();
       Row();
+      writeOptions();
+    }
+
+    public static void writeOptions(){
+      Console.SetCursorPosition(3,2);
+      Console.WriteLine("Editor HTML");
+
+      Console.SetCursorPosition(3,3);
+      Console.WriteLine("----------------");
+
+      Console.SetCursorPosition(3,4);
+      Console.WriteLine("Selecione uma opção a baixo:");
+
+      Console.SetCursorPosition(3,6);
+      Console.WriteLine("1 - Novo arquivo");
+
+      Console.SetCursorPosition(3,7);
+      Console.WriteLine("2 - Abrir");
+
+      Console.SetCursorPosition(3,9);
+      Console.WriteLine("0 - Sair");
+
+      Console.SetCursorPosition(7,10);
+      Console.WriteLine("Opcão: ");
     }
 
   }
